1621e4b828b64930685b3b770a2bb8dbd9df87ac
samantha van damme <skvdamme@gmail.com>
2017-07-26 17:57:19 +0200

    last commit

--- app.js
+++ app.js
@@ -1,30 +1,5 @@
 // User Information App - Web Server
 
-// Create a Node.js application that is the beginning of a user management system. 
-// Your users are all saved in a "users.json" file, and you can currently do the following:
-// - search for users
-// - add new users to your users file.
-// - get your starter file here: users.jsonView in a new window
-
-// Part 0
-// Create one route:
-// - route 1: renders a page that displays all your users.
-
-
-// Part 1
-// Create two more routes:
-// - route 2: renders a page that displays a form which is your search bar.
-// - route 3: takes in the post request from your form, then displays matching users on a new page. 
-//   Users should be matched based on whether either their first or last name contains the input string.
-
-// Part 2
-// Create two more routes:
-// - route 4: renders a page with three forms on it (first name, last name, and email) 
-// that allows you to add new users to the users.json file.
-// - route 5: takes in the post request from the 'create user' form, 
-// then adds the user to the users.json file. 
-// Once that is complete, redirects to the route that displays all your users (from part 0).
-
 var express = require('express');
 var app = express();
 var fs = require('fs');
@@ -60,21 +35,28 @@ app.get('/searchBar', function (req, res) {
 });
 
 app.post('/ajax', function (req, res){
-    fs.readFile('users.json', function(err, data){
+   
+    var search = req.body.theresult;
+    
+
+    fs.readFile('users.json', 'utf8', function(err, data){
         if(err){
             throw err;
         }
     var json = JSON.parse(data);
-    var search = req.body.result;
     var empty = "";
     var array = [];
-        for (var x = 0; x < json.length; x++){
-            if (object1 === empty){
+    
+    for(var x = 0; x < json.length; x++){
+        if(search === empty){
+                console.log("nothing")
 
-            } else if(json[x].firstname.includes(search) || json[x].lastname.includes(search) || json[x].email.includes(search)){
-                var data1 = json[x].firstname + " " + json[x].lastname + " " + json[x].email;
-                array.push(data1);
-            }
+        } else if(json[x].firstname.includes(search) || json[x].lastname.includes(search) || json[x].email.includes(search)){
+            var data1 = json[x].firstname + " " + json[x].lastname + " " + json[x].email;
+            array.push(data1);
+            console.log(data1);
+        }
+        
         };
         res.send(array);
     });
